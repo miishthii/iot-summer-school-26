8d21d6882577ddd427262bff7825878aa0d0a8b7
miishthii <24bec042@smvdu.ac.in>
2026-06-27 00:04:50 +0530

    Add files via upload

---
+++ IoT Week2/IRSensorDoor.ino
@@ -0,0 +1,16 @@
+#include<Servo.h>
+int led = 13;
+
+void setup()
+{ pinMode(led, OUTPUT);
+  Wire.begin(8);
+  Wire.onReceive(receive);}
+
+void loop(){}
+
+void receive(int bytes)
+{int x = Wire.read();
+ if(x == 1){
+  digitalWrite(led, HIGH);}
+ else{
+  digitalWrite(led, LOW);}}
