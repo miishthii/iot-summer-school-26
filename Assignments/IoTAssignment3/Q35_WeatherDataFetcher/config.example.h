ac0b5a76bf574e318649a7a390eb8fc8206a9c21
Mishthi Chopra <24bec042@smvdu.ac.in>
2026-07-05 14:10:16 +0530

    Create config.example.h
    feat: add example config code

---
+++ Assignments/IoTAssignment3/Q35_WeatherDataFetcher/config.example.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* WIFI_SSID = "YOUR_WIFI";
+const char* WIFI_PASSWORD = "YOUR_PASSWORD";
+const char* API_KEY = "YOUR_API_KEY";
+const char* CITY = "Jammu";
+
+#endif
